8ba836d45f6a484d9846c9c81896772c314ece7a
TYWong89 <tywong89@gmail.com>
2025-05-05 13:51:23 -0700

    Home page created, fetch function works for random

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Login/>}/>
-        <Route path='/home/:Username' element={<Home/>}/>
+        <Route path='/home/' element={<Home/>}/>
       </Routes>
     </Router>
   )
